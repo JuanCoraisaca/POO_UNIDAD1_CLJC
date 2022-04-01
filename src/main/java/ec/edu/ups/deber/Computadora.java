6e835783309534480ec5e2fb0e03129b0f0f058c
usuario <usuario@localhost>
2022-03-31 21:27:43 -0500

    Deber clases y objetos

---
+++ src/main/java/ec/edu/ups/deber/Computadora.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.deber;
+
+/**
+ *
+ * @author usuario
+ */
+public class Computadora {
+    String marca;
+    double precio;
+    String color;
+    float peso;
+    
+    
+}
---
+++ src/main/java/ec/edu/ups/deber/Restaurant.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.deber;
+
+import java.util.Date;
+
+/**
+ *
+ * @author usuario
+ */
+public class Restaurant {
+    String cliente;
+    int precio;
+    String direccion;
+    Date horario;
+    
+    
+    
+}
